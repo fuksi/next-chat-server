ec437055b923525e63f26f75d1492cc4a086b64e
fuksi <phuc.trandt@outlook.com>
2020-04-26 01:55:42 +0300

    Add .net core api with a sample signalr hub

---
+++ NextChat.ChatApi/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace NextChat.ChatApi.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "foo", "bar");
+        }
+    }
+}
